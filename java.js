ca7d5c5f6ec51c668945533500b1e2e3a5a4c831
Cecília <mariacecilialima638@gmail.com>
2026-07-24 18:57:04 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,17 @@
+
+console.log ("Bem vindo!")
+alert("Seja Bem Vindo!")
+
+
+let nome = prompt("Qual seu nome?")
+console.log ("Olá" +   nome)
+
+let cidade = prompt("Dequal cidade você é?")
+console.log (cidade)
+
+
+let numero = prompt (" Adivinhe o número secreto.")
+if (numeo !== "7" ){
+    alert("Aprovado")
+} else {
+    alert ("Reprovado")}
